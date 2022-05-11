08db4f42bcf7805ac81af0e5a9268e36680d994b
djkek <64024016+djkek@users.noreply.github.com>
2022-05-11 17:18:17 +0200

    500kHz, receive_temp i inne

--- PUL-temp.srcs/sources_1/new/top.vhd
+++ PUL-temp.srcs/sources_1/new/top.vhd
@@ -1,43 +1,112 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 09.05.2022 22:30:30
--- Design Name: 
--- Module Name: top - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
+-- PUL Projekt - Dominik Jankowski 250184
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity top is
---  Port ( );
+Port (
+     Clock100MHz : in std_logic;
+     SW0 : in std_logic;
+     ADC_DOUT : in std_logic;
+
+     ADC_CLK : out std_logic;
+     ADC_CS : out std_logic;
+     PWM : out std_logic
+     );
 end top;
 
 architecture Behavioral of top is
+type stany is (Idle, Init, Heating, Measure, Calculate, Maintain);
+signal State, StateNext : stany := Idle;
+
+signal timecounter : std_logic_vector(10 downto 0) := (others => '0');
+signal timedone : std_logic := '0';
+
+signal bitcounter : std_logic_vector(3 downto 0) := (others => '0');
+signal bitdone : std_logic := '0';
+
+signal temp : unsigned(11 downto 0) := (others => '-');
+
+signal pwm_period : std_logic_vector(10 downto 0) := "11111111111";
+signal pwm_ff : std_logic_vector(5 downto 0) := "111111";
+
+signal Clock500kHz : std_logic := '0';
+signal clockcounter : std_logic_vector(7 downto 0) := (others => '0');
+
+signal PWM_OUT : std_logic := '0';
+
 
 begin
 
+clockgen: process(Clock100MHz)
+begin
+if rising_edge(Clock100MHz) then
+    clockcounter <= clockcounter + "01";
+    if clockcounter = "11001000" then
+        Clock500kHz <= not Clock500kHz;
+        clockcounter <= (others => '0');
+    end if;
+end if;
+end process clockgen;
+
+
+reg: process(Clock100MHz)
+begin
+if SW0 = '0' then
+    State <= Idle;
+elsif SW0 = '1' then
+    if rising_edge(Clock100MHz) then
+        State <= StateNext;
+    end if;
+end if;
+end process reg;
+
+
+wyjscia: process(Clock100MHz)
+begin
+    case state is
+        when Idle =>
+            ADC_CLK <= '0';
+            ADC_CS <= '1';
+            PWM <= '0';
+            timecounter <= (others => '0');
+            bitcounter <= (others => '0');
+            temp <= (others => '0');
+            
+        when Init =>
+            ADC_CS <= '1';
+        when Heating =>
+            ADC_CS <= '1';
+            PWM <= PWM_OUT;
+        when Measure =>
+            ADC_CLK <= Clock500kHz;
+            ADC_CS <= '0';
+        when Calculate =>
+            ADC_CS <= '1';
+        when Maintain =>
+            ADC_CS <= '1';
+    end case;
+end process wyjscia;
+
+
+receive_temp: process(Clock500kHz)
+begin
+if rising_edge(Clock500kHz) then
+    if State = Measure then
+        if bitdone = '0' then
+                bitcounter <= bitcounter + "01";
+                if bitcounter > 3 then
+                    temp <= shift_left(temp, 1);
+                    temp(11) <= ADC_DOUT;
+                end if;
+                if bitcounter = 15 then
+                    bitdone <= '1';
+                end if;
+        end if;
+    end if;
+end if;
+end process receive_temp;
 
 end Behavioral;
--- PUL_Arty_Shield.xdc
+++ PUL_Arty_Shield.xdc
@@ -26,7 +26,7 @@ set_property -dict { PACKAGE_PIN H6    IOSTANDARD LVCMOS33 } [get_ports { LED3_G
 set_property -dict { PACKAGE_PIN K1    IOSTANDARD LVCMOS33 } [get_ports { LED3_R }]; #IO_L23N_T3_35 Sch=LED3_r
 
 ## Switches
-set_property -dict { PACKAGE_PIN A8    IOSTANDARD LVCMOS33 } [get_ports { SW[0] }]; #IO_L12N_T1_MRCC_16 Sch=sw[0]
+set_property -dict { PACKAGE_PIN A8    IOSTANDARD LVCMOS33 } [get_ports { SW0 }]; #IO_L12N_T1_MRCC_16 Sch=sw[0]
 set_property -dict { PACKAGE_PIN C11   IOSTANDARD LVCMOS33 } [get_ports { SW[1] }]; #IO_L13P_T2_MRCC_16 Sch=sw[1]
 set_property -dict { PACKAGE_PIN C10   IOSTANDARD LVCMOS33 } [get_ports { SW[2] }]; #IO_L13N_T2_MRCC_16 Sch=sw[2]
 set_property -dict { PACKAGE_PIN A10   IOSTANDARD LVCMOS33 } [get_ports { SW[3] }]; #IO_L14P_T2_SRCC_16 Sch=sw[3]
