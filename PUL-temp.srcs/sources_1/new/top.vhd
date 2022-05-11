16f41127c9461d6d1f44b8632f583b7bbe9566ef
djkek <64024016+djkek@users.noreply.github.com>
2022-05-11 20:37:57 +0200

    Logika i lampki

--- PUL-temp.srcs/sources_1/new/top.vhd
+++ PUL-temp.srcs/sources_1/new/top.vhd
@@ -13,12 +13,17 @@ Port (
 
      ADC_CLK : out std_logic;
      ADC_CS : out std_logic;
-     PWM : out std_logic
+     PWM : out std_logic;
+     LED0 : out std_logic; -- low temp
+     LED1 : out std_logic;
+     LED2 : out std_logic;
+     LED3 : out std_logic; -- docelowa temp
+     LED0_R : out std_logic -- powy?ej docelowej
      );
 end top;
 
 architecture Behavioral of top is
-type stany is (Idle, Init, Heating, Measure, Calculate, Maintain);
+type stany is (Idle, Init, Heating, Measure, Correction, Maintain);
 signal State, StateNext : stany := Idle;
 
 signal timecounter : std_logic_vector(10 downto 0) := (others => '0');
@@ -28,6 +33,7 @@ signal bitcounter : std_logic_vector(3 downto 0) := (others => '0');
 signal bitdone : std_logic := '0';
 
 signal temp : unsigned(11 downto 0) := (others => '-');
+signal temp_old : unsigned(11 downto 0) := (others => '-');
 
 signal pwm_period : std_logic_vector(10 downto 0) := "11111111111";
 signal pwm_ff : std_logic_vector(5 downto 0) := "111111";
@@ -37,6 +43,7 @@ signal clockcounter : std_logic_vector(7 downto 0) := (others => '0');
 
 signal PWM_OUT : std_logic := '0';
 
+signal a, b : unsigned(3 downto 0) := "0000";
 
 begin
 
@@ -80,15 +87,75 @@ begin
         when Heating =>
             ADC_CS <= '1';
             PWM <= PWM_OUT;
+            if temp > 25 then
+                LED0 <= '1';
+            end if;
+            if temp > 30 then
+                LED1 <= '1';
+            end if;
+            if temp > 35 then
+                LED2 <= '1';
+            end if;
+            if temp > 42 then
+                LED0_R <= '1';
+            end if;
+            
         when Measure =>
             ADC_CLK <= Clock500kHz;
             ADC_CS <= '0';
-        when Calculate =>
+        when Correction =>
             ADC_CS <= '1';
         when Maintain =>
             ADC_CS <= '1';
+            LED3 <= '1';
     end case;
 end process wyjscia;
+
+przejscia: process(Clock100MHz)
+begin
+if rising_edge(Clock100Mhz) then
+    StateNext <= State;
+    if SW0 = '0' then
+        StateNext <= Idle;
+    else
+    case State is
+        when Idle =>
+            if SW0 = '1' then
+                StateNext <= Init;
+            end if;
+            
+        when Init =>
+            StateNext <= Heating;
+            
+        when Heating => -- tu tylko kontrolujemy predkosc narostu wzgledem odleglosci
+            if timedone = '1' then
+                StateNext <= Measure;
+            end if;
+            
+        when Measure =>
+            if bitdone = '1' then
+                StateNext <= Correction;
+            end if;
+            
+        when Correction =>
+            -- dostaje info o temperaturze obecnej
+            -- je?eli w dobrym zakresie -> maintain
+            -- je?eli w z?ym -> liczy predkosc narostu
+            -- je?eli ok -> heating bez korekty
+            -- je?eli nie -> korekta PWM a potem heating
+            
+            -- (30 - temp)/((temp-tempold)/okres)
+            
+        when Maintain =>
+            if temp > 38 and temp < 42 then
+                StateNext <= Maintain;
+            else
+                StateNext <= Heating;
+            end if;
+    end case;
+    end if;
+end if;
+end process przejscia;
 
 
 receive_temp: process(Clock500kHz)
@@ -105,6 +172,8 @@ if rising_edge(Clock500kHz) then
                     bitdone <= '1';
                 end if;
         end if;
+    elsif State = Correction then
+        bitdone <= '0';
     end if;
 end if;
 end process receive_temp;
