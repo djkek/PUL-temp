bd253cfbb80ec16404c98a22712307288991ecbc
djkek <64024016+djkek@users.noreply.github.com>
2022-05-12 21:31:29 +0200

    PWM i inne

--- PUL-temp.srcs/sources_1/new/top.vhd
+++ PUL-temp.srcs/sources_1/new/top.vhd
@@ -35,15 +35,25 @@ signal bitdone : std_logic := '0';
 signal temp : unsigned(11 downto 0) := (others => '-');
 signal temp_old : unsigned(11 downto 0) := (others => '-');
 
-signal pwm_period : std_logic_vector(10 downto 0) := "11111111111";
-signal pwm_ff : std_logic_vector(5 downto 0) := "111111";
+signal pwm_period : std_logic_vector(16 downto 0) := "11000011010100000";
+signal pwm_ff : std_logic_vector(16 downto 0) := (others => '0');
+signal pwm_counter : std_logic_vector(16 downto 0) := (others => '0');
 
 signal Clock500kHz : std_logic := '0';
 signal clockcounter : std_logic_vector(7 downto 0) := (others => '0');
 
 signal PWM_OUT : std_logic := '0';
 
-signal a, b : unsigned(3 downto 0) := "0000";
+signal slope_target : std_logic_vector(7 downto 0) := (others => '0');
+signal slope : std_logic_vector(7 downto 0) := (others => '0');
+-- jednostka - binarny odpopwiednik stopni na 1 pwm_period
+-- np. 1 stopien C /s wychodzi
+-- 19,5 = oko?o 20
+-- czyli 10100
+--
+-- je?eli liczymy co 2 s to mamy ju? 40 bo mamy 2 stopnie ró?nicy
+-- zatem trzeba znormalizowa?
+
 
 begin
 
@@ -58,7 +68,6 @@ if rising_edge(Clock100MHz) then
 end if;
 end process clockgen;
 
-
 reg: process(Clock100MHz)
 begin
 if SW0 = '0' then
@@ -70,6 +79,19 @@ elsif SW0 = '1' then
 end if;
 end process reg;
 
+count_time: process(Clock100MHz)
+begin
+if rising_edge(Clock100MHz) then
+    if State = Heating then
+        timecounter <= timecounter + "01";
+        if timecounter = "11111111111" then
+            timedone <= '1';
+            timecounter <= (others => '0');
+        end if;
+    else timedone <= '0';
+    end if;
+end if;
+end process count_time;
 
 wyjscia: process(Clock100MHz)
 begin
@@ -78,14 +100,16 @@ begin
             ADC_CLK <= '0';
             ADC_CS <= '1';
             PWM <= '0';
-            timecounter <= (others => '0');
-            bitcounter <= (others => '0');
-            temp <= (others => '0');
             
         when Init =>
             ADC_CS <= '1';
+            ADC_CLK <= '0';
+            
+            
+            
         when Heating =>
             ADC_CS <= '1';
+            ADC_CLK <= '0';
             PWM <= PWM_OUT;
             if temp > 25 then
                 LED0 <= '1';
@@ -103,10 +127,17 @@ begin
         when Measure =>
             ADC_CLK <= Clock500kHz;
             ADC_CS <= '0';
+            
         when Correction =>
+            ADC_CLK <= '0';
             ADC_CS <= '1';
+            
         when Maintain =>
+            ADC_CLK <= '0';
             ADC_CS <= '1';
+            LED0 <= '1';
+            LED1 <= '1';
+            LED2 <= '1';
             LED3 <= '1';
     end case;
 end process wyjscia;
@@ -127,7 +158,7 @@ if rising_edge(Clock100Mhz) then
         when Init =>
             StateNext <= Heating;
             
-        when Heating => -- tu tylko kontrolujemy predkosc narostu wzgledem odleglosci
+        when Heating => -- tu tylko kontrolujemy slopa
             if timedone = '1' then
                 StateNext <= Measure;
             end if;
@@ -138,9 +169,15 @@ if rising_edge(Clock100Mhz) then
             end if;
             
         when Correction =>
+            if temp > 38 and temp < 42 then
+                StateNext <= Maintain;
+            else
+                StateNext <= Heating;
+            end if;
+            
             -- dostaje info o temperaturze obecnej
             -- je?eli w dobrym zakresie -> maintain
-            -- je?eli w z?ym -> liczy predkosc narostu
+            -- je?eli w z?ym -> liczy slopa
             -- je?eli ok -> heating bez korekty
             -- je?eli nie -> korekta PWM a potem heating
             
@@ -157,25 +194,50 @@ if rising_edge(Clock100Mhz) then
 end if;
 end process przejscia;
 
-
 receive_temp: process(Clock500kHz)
 begin
 if rising_edge(Clock500kHz) then
     if State = Measure then
         if bitdone = '0' then
-                bitcounter <= bitcounter + "01";
-                if bitcounter > 3 then
-                    temp <= shift_left(temp, 1);
-                    temp(11) <= ADC_DOUT;
-                end if;
-                if bitcounter = 15 then
-                    bitdone <= '1';
-                end if;
+            if bitcounter = 0 then
+                temp_old <= temp;
+            end if;
+            bitcounter <= bitcounter + "01";
+            if bitcounter > 3 then
+                temp <= shift_left(temp, 1);
+                temp(11) <= ADC_DOUT;
+            end if;
+            if bitcounter = 15 then
+                bitdone <= '1';
+                bitcounter <= (others => '0');
+            end if;
         end if;
     elsif State = Correction then
         bitdone <= '0';
     end if;
 end if;
 end process receive_temp;
+
+Calculate_Slope: process(State)
+begin
+if rising_edge(Clock100MHz) and State = Correction and temp_old /= ("------------") then
+    
+    pwm_ff <= (others => '0'); -- korekta pwm
+end if;
+end process Calculate_Slope;
+
+PWM_gen: process(Clock100MHz)
+begin
+if rising_edge(Clock100MHz) and State /= Idle then
+    pwm_counter <= pwm_counter + "01";
+    if pwm_counter < pwm_ff then
+        PWM_OUT <= '1';
+    elsif pwm_counter < pwm_period then
+        PWM_OUT <= '0';
+    elsif pwm_counter = pwm_period then
+        pwm_counter <= (others => '0');
+    end if;
+end if;
+end process PWM_gen;
 
 end Behavioral;
